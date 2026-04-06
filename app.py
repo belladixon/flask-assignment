96e4e5b8dde5a6f965c130298b0ae54ff2f70d06
Bella Dixon <idxon@uiowa.edu>
2026-04-06 16:54:31 -0500

    added abput page

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
